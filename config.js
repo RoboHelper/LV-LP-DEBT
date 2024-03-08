1514173003c0b8a9dda9d1fab238aa9af2c14e2a
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-03-09 01:40:35 +0500

    new page style ready

---
+++ config.js
@@ -0,0 +1,17 @@
+const domainName = "ushelpcentre.com";
+
+const G2DisplayName = "Portland Benefits Group LLC";
+const G2Link =
+  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=1241";
+
+const addLeadId = false;
+
+const qualifyNumber = "+18338891275";
+const overflowNumber = "+18445942134";
+
+const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
+
+const googleAccountConversionTagID = "AW-11108069111";
+const googleConversionTagEventSentTo = "AW-11108069111/a-GaCNvSko8ZEPfd3rAp";
+const googleConversionTagValue = 15.0;
